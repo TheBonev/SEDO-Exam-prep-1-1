94c25fd6cecd823fd7361d19f66467cb364e27e8
TheBonev <gbb97@abv.bg>
2025-10-25 15:18:29 +0300

    change

--- script.js
+++ script.js
@@ -18,3 +18,13 @@ document.getElementById('btn-subtract').addEventListener('click', () => {
   const b = Number(document.getElementById('b').value);
   document.getElementById('result').textContent = subtract(a, b);
 });
+
+//Change 3
+
+
+
+
+//change 4
+
+
+//change 6
